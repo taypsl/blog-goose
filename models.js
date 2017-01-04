d9eb35dc12613ec64c0b30bc4960670062ff1c34
Taylor Parsell <tayparsell@gmail.com>
2017-01-04 00:41:12 -0800

    added instance method for APIreturn

--- models.js
+++ models.js
@@ -3,9 +3,27 @@ const mongoose = require('mongoose');
 // schema
 const blogSchema = mongoose.Schema({
 	title: {type: String, required: true},
-	address: {
+	author: {
     	firstName: String,
     	lastName: String
     }	
     content: {type: String, required: true},
-})
+});
+
+//return author name as a human-readable string
+blogSchema.virtual('authorString').get(function() {
+	return `${this.author.firstName} ${this.author.lastName}`.trim()
+});
+
+// create instance method to create object for API to return
+restaurantSchema.methods.apiRepr = function() {
+	return {
+	    id: this._id,
+	    title: this.title,
+	    author: this.authorString
+	};
+}
+
+const BlogPost = mongoose.model('BlogPost', blogSchema);
+
+module.exports = {BlogPost};
