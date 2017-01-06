daed6dd581921ecc7a9adc531f876d3f703de8a2
Taylor Parsell <tayparsell@gmail.com>
2017-01-05 16:45:13 -0800

    updating database url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/grilledcheese';
+                      'ds019708.mlab.com:19708/grilledcheese';
 exports.PORT = process.env.PORT || 8080;
