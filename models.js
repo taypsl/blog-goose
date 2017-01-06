84bdf3599f1f0de0c722e5dd907ddff2a0bc42db
Taylor Parsell <tayparsell@gmail.com>
2017-01-06 00:36:59 -0800

    fixed syntax error

--- models.js
+++ models.js
@@ -1,27 +1,27 @@
 const mongoose = require('mongoose');
 
-// schema
+// this is our schema to represent a restaurant
 const blogSchema = mongoose.Schema({
-	title: {type: String, required: true},
-	author: {
-    	firstName: String,
-    	lastName: String
-    }	
-    content: {type: String, required: true}
+  title: {type: String, required: true},
+  author: {
+  	firstName: String, 
+  	lastName: String
+  },
+  content: {type: String, required: true}
 });
 
-//return author name as a human-readable string
+// return author name as human readable string
 blogSchema.virtual('authorString').get(function() {
-	return `${this.author.firstName} ${this.author.lastName}`.trim()
+  return `${this.author.firstName} ${this.author.lastName}`.trim()
 });
 
-// create instance method to return object that exposes limited resource to API
+// create *instance method to return object that exposes limited resource to API
 blogSchema.methods.apiRepr = function() {
-	return {
-	    id: this._id,
-	    title: this.title,
-	    author: this.authorString
-	};
+  return {
+    id: this._id,
+    title: this.title,
+    author: this.authorString,
+  };
 }
 
 const BlogPost = mongoose.model('BlogPost', blogSchema);
